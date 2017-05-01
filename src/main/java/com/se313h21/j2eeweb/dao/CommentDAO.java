eff8dd23ce69850742c1950c0d2f057901a6da9f
luannt <luannt2909@gmail.com>
2017-05-01 09:57:18 +0700

    add feature comment

---
+++ src/main/java/com/se313h21/j2eeweb/dao/CommentDAO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se313h21.j2eeweb.dao;
+
+import com.se313h21.j2eeweb.model.Comment;
+import com.se313h21.j2eeweb.model.Post;
+import com.se313h21.j2eeweb.model.User;
+import com.se313h21.j2eeweb.repositories.CommentRepository;
+import com.se313h21.j2eeweb.repositories.PostRepository;
+import com.se313h21.j2eeweb.repositories.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Nguyen Tan Luan
+ */
+@Service
+public class CommentDAO {
+    @Autowired
+    CommentRepository commentRepo;
+    
+    @Autowired
+    UserRepository userRepo;
+    
+    @Autowired
+    PostRepository postRepo;
+    
+    public List<Comment> getMany(User user){
+        List<Comment> listComment=commentRepo.findByUserId(user.getId());
+        return listComment;
+    }
+    
+    public List<Comment> getMany(Post post){
+        List<Comment> listComment=commentRepo.findByPostId(post.getId());
+        return listComment;
+    }
+    
+    public Comment create(String content, User user, Post post){
+        Comment comment=new Comment(-1,content);
+        comment.setUserId(user);
+        comment.setPostId(post);
+        commentRepo.save(comment);
+        return comment;
+    }
+    public Comment update(Comment comment){
+        return commentRepo.save(comment);
+    }
+    public void Delete(Comment comment){
+        commentRepo.delete(comment);
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/se313h21/j2eeweb/repositories/CommentRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se313h21.j2eeweb.repositories;
+
+import com.se313h21.j2eeweb.model.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Nguyen Tan Luan
+ */
+public interface CommentRepository extends JpaRepository<Comment, Integer>{
+    public List<Comment> findByUserId(Integer userId);
+    public List<Comment> findByPostId(Integer postId);
+    
+}
